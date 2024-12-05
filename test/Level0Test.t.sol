864bc2cdf23cff32b8c9165b9445e4c9b169c3af
GaurangBharadava <gaurangbrdv1357@gmail.com>
2024-12-05 18:54:36 +0530

    Level0-challenge

---
+++ test/Level0Test.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {Instance} from "../src/Level0.sol";
+import {Level0Solution} from "../script/Level0Solution.s.sol";
+contract Level0Test is Test {
+
+    Instance public instance;
+    Level0Solution public deployer;
+
+    string private password = "ok";
+
+    function setUp() public {
+        instance = new Instance(password);
+        deployer = new Level0Solution();
+    }
+
+    function testAuthenticate() public {
+        instance.authenticate(password);
+        assertEq(true,instance.getCleared());
+    }
+}
