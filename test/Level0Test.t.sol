119239286dcfa9c78f7092a8b3581136619bd91a
GaurangBharadava <gaurangbrdv1357@gmail.com>
2024-12-05 18:55:49 +0530

    level0-challenge

--- test/Level0Test.t.sol
+++ test/Level0Test.t.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import {Test} from "../lib/forge-std/src/Test.sol";
 import {Instance} from "../src/Level0.sol";
 import {Level0Solution} from "../script/Level0Solution.s.sol";
-contract Level0Test is Test {
 
+contract Level0Test is Test {
     Instance public instance;
     Level0Solution public deployer;
 
@@ -18,6 +18,6 @@ contract Level0Test is Test {
 
     function testAuthenticate() public {
         instance.authenticate(password);
-        assertEq(true,instance.getCleared());
+        assertEq(true, instance.getCleared());
     }
 }
